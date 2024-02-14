2cf3a208593cf35f86c6e69157224f917b8665ff
Dharstec <info@dharstec.com>
2024-02-14 17:43:38 +0530

    UI changes and navbar search field only for all cases

--- src/app/components/institution/institution.component.ts
+++ src/app/components/institution/institution.component.ts
@@ -11,12 +11,12 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class InstitutionComponent {
   members: {title: string, key: string, lat: number,lon:number, url: string}[] = [
-    {title: 'NORTH AMERICA', key: 'northamerica', lat:7.18805555556,lon:21.0936111111, url: '../../../assets/na.png'},
-    {title: 'EUROPE', key: 'europe', lat:7.18805555556,lon:21.0936111111, url: '../../../assets/europe.png'},
-    {title: 'ASIA', key: 'asia',  lat:7.18805555556,lon:21.0936111111, url: '../../../assets/asia.png'},
-    {title: 'SOUTH AMERICA', key: 'southamerica',  lat:7.18805555556,lon:21.0936111111, url: '../../../assets/sa.png'},
-    {title: 'AFRICA', key: 'africa', lat:7.18805555556,lon:21.0936111111, url: '../../../assets/africa.png'},
-    {title: 'AUSTRALIA', key: 'australia',  lat:7.18805555556,lon:21.0936111111, url: '../../../assets/australia.png'},
+    {title: 'NORTH AMERICA', key: 'northamerica', lat:7.18805555556,lon:21.0936111111, url: '../../../assets/na1.png'},
+    {title: 'EUROPE', key: 'europe', lat:7.18805555556,lon:21.0936111111, url: '../../../assets/europe1.png'},
+    {title: 'ASIA', key: 'asia',  lat:7.18805555556,lon:21.0936111111, url: '../../../assets/asia1.png'},
+    {title: 'SOUTH AMERICA', key: 'southamerica',  lat:7.18805555556,lon:21.0936111111, url: '../../../assets/sa1.png'},
+    {title: 'AFRICA', key: 'africa', lat:7.18805555556,lon:21.0936111111, url: '../../../assets/africa1.png'},
+    {title: 'OCEANIA', key: 'australia',  lat:7.18805555556,lon:21.0936111111, url: '../../../assets/australia1.png'},
   ]
 
   constructor(private api: ApiService, public dialog: MatDialog, private snackbar: MatSnackBar, private router: Router) { }
