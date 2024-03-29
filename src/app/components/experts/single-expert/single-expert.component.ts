ebd9c8d8f85dc6176d3ae78c388983119795c8ba
Dharstec <info@dharstec.com>
2024-03-29 15:49:35 +0530

    operators integration

--- src/app/components/experts/single-expert/single-expert.component.ts
+++ src/app/components/experts/single-expert/single-expert.component.ts
@@ -4,69 +4,63 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, ActivatedRoute } from '@angular/router';
 import { faInstagram, faLinkedin, faTwitter, faFacebook, faXTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { MatCardModule } from '@angular/material/card';
 import { DomSanitizer } from '@angular/platform-browser';
 import { FormControl } from '@angular/forms';
-import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-single-expert',
   templateUrl: './single-expert.component.html',
   styleUrls: ['./single-expert.component.scss']
 })
-export class SingleExpertComponent {
+export class SingleExpertComponent implements OnInit {
   facebook = faFacebook;
   instagram = faInstagram;
   twitterX = faXTwitter;
   linkedIn = faLinkedin;
 
-  operators_id: any
-  single_ops_details : any;
-  instituteList: any= [];
+  operators_id: any;
+  single_ops_details: any;
+  instituteList: any = [];
   institutes = new FormControl();
-  // instituteFilterControl = new FormControl();
   filteredInstituteList: any[];
   instituteSearchControl = new FormControl();
-  
+
   operators: any;
   filterdOperators: any;
   id: any;
+
   constructor(private api: ApiService, public dialog: MatDialog, private _sanitizer: DomSanitizer, private snackbar: MatSnackBar, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.operators_id= this.route.snapshot.paramMap.get('id')
-    this.getInstituteList()
-    this.getOperatorsList()
-    // console.log("--",this.operators)
+    this.operators_id = this.route.snapshot.paramMap.get('id');
+    this.getOperatorsList();
   }
 
   getOpsDetails(): void {
     this.api.apiGetDetailsCall(this.operators_id, 'operator').subscribe((data) => {
       this.single_ops_details = data.data;
-      console.log(this.single_ops_details)
-    })
+      console.log(this.single_ops_details);
+    });
   }
-  routeToSingleOps(opsId) {
-    this.router.navigate(['/user/all-cases/single-case',opsId])
-    this.operators_id =opsId
-    this.getOpsDetails()
-    window.scrollTo(0, 0);
+  routeToSingleOps(opsId): void {
+    this.operators_id = opsId;
+    this.router.navigate(['/user/all-cases/single-case', opsId]).then(() => {
+      this.getOpsDetails();
+      window.scrollTo(0, 0);
+    });
   }
-  // ---------
-  getInstituteList() {
-    this.api.apiGetCall('institute').subscribe((data) => {
-      this.instituteList = data.data;
-      this.filteredInstituteList = this.instituteList;
-    })
-    
-  }
-  getOperatorsList() {
+
+  getOperatorsList(): void {
     this.api.apiGetCall('operator').subscribe((data) => {
       this.operators = data.data;
-      this.filterdOperators = data.data;
-      // console.log(this.operators)
-    })
+      // Filter the operators to find the single operator by ID
+      const singleOperator = this.operators.find(operator => operator._id === this.operators_id);
+      if (singleOperator) {
+        this.single_ops_details = singleOperator;
+        console.log(this.single_ops_details);
+      } else {
+        console.error('Single operator not found');
+      }
+    });
   }
-  
 }
